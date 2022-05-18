a413c83ce3e136b205b6850010827d7105606c11
lincoln <lincoln-md@hotmail.com>
2022-05-18 16:29:50 -0300

    first commit

---
+++ frontend/src/views/Home/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2022</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
+export default defineComponent({
+  components: {
+    CustomHeader,
+    Contact
+  },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin () {
+      console.log('')
+    }
+
+    function handleAccountCreate () {
+      console.log('')
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+})
+</script>
+
+<style>
+</style>
