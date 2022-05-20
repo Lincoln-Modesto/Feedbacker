0bcc257806098b33ebd0323db8c2232762219201
lincoln <lincoln-md@hotmail.com>
2022-05-20 18:28:12 -0300

    feat: construindo a landingpage

---
+++ frontend/src/views/Home/CustomHeader.vue
@@ -0,0 +1,80 @@
+<template>
+  <header class="header">
+    <div class="header-group">
+      <div class="flex items-center justify-between py-10">
+        <div class="w-28 lg:36">
+          <img
+            src="../../assets/images/logo_white.png"
+            class="w-full"
+            alt="logo"
+          />
+        </div>
+
+        <div class="flex">
+          <button class="font-bold px-6 py-2 text-white focus:outline-none">
+            Crie uma conta
+          </button>
+          <button
+            class="
+              font-bold
+              px-6
+              py-2
+              bg-white
+              rounded-full
+              text-brand-main
+              focus:outline-none
+            "
+          >
+            Entrar
+          </button>
+        </div>
+      </div>
+      <div class="flex flex-col mt-28">
+        <h1 class="text-4xl font-black text-white">Tenha um Feedback. <br>
+        E faça seus clientes mais <br class="hidden lg:inline-block">
+        felizes!
+        </h1>
+        <p class="text-white text-lg mt-5">Receba ideias reclamações e feedbacks com um <br class="hidden lg:inline-block">
+         simples widget na página.</p>
+        <div>
+          <button class="
+              font-bold
+              px-6
+              py-2
+              mt-5
+              bg-white
+              rounded-full
+              text-brand-main
+              focus:outline-none
+            ">
+            Crie uma conta grátis
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+<style lang="postcss" scoped>
+.header {
+  @apply bg-brand-main w-full flex justify-center;
+  height: 700px;
+}
+
+.header-group{
+  @apply flex flex-col w-4/5 max-w-6xl;
+}
+
+@media (min-width: 640px) {
+  .header-group{
+    background-image: url(../../assets/images/blue_balloons.png);
+    background-size: 628px;
+    background-position: 90% 100%;
+    background-repeat: no-repeat;
+  }
+}
+</style>
